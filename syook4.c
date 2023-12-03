51021ccbfaaf92c4d77ac0321965f9549fac993f
barath <152768707+Anbubarath@users.noreply.github.com>
2023-12-03 16:06:14 +0530

    Add files via upload

---
+++ syook4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<stdbool.h>
+#define GPIO_BASE_ADDRESS 0x40000000
+#define GPIO_DIR__OFFSET 0x04
+void blink_LED()
+{
+volatile uint32_t *gpio_base=(volatile uint32_t *)GPIO_BASE_ADDRESS;
+gpio_base[GPIO_DIR_OFFSET] |=0X01;
+while (true)
+{
+gpio_base[GPIO_DATA_OFFSET] ^=0X01;
+for(volatile uint32 i=0;i<1000000;i++);
+}
+}
+int main()
+{
+blink_LED();
+return 0;
+}
+
